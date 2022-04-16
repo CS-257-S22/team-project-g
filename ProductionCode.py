f6bae1a70dc2a8a7b8ec96bac82f84edb1616708
DakePeng <pengd@carleton.edu>
2022-04-15 19:07:40 -0500

    Cleaned up retrieveData()

--- ProductionCode.py
+++ ProductionCode.py
@@ -2,33 +2,35 @@ import sys
 import csv
 import os
 
-dataSet = []
+relativeDataPath = "Data/dummy_data.csv" #data path of the dataset .csv file relative to this folder
+dataSet = [] #2-d array data set in the format [Date, County, State, Confirmed Cases, Confirmed Deaths]
 
-def retrieveData(filePath):
+def retrieveData():
+    absoluteDataPath = convertRealtivePathToAbsolutePath()
+    stringLines = getStringLinesFromFile(absoluteDataPath)
+    storeData(stringLines)
+
+def convertRealtivePathToAbsolutePath():
+    currentPath = os.path.dirname(__file__)
+    return os.path.join(currentPath, relativeDataPath)
+
+def getStringLinesFromFile(filePath):
     with open(filePath) as file:
         stringLines  = file.readlines()[1:]
+    file.close()
+    return stringLines
+
+def storeData(stringLines):
     for i in stringLines:
         arrayLine = convertStringLinetoArray(i)
+        arrayLine.pop() # remove last column
         dataSet.append(arrayLine)
-        arrayLine.pop()
-    file.close()
 
 def convertStringLinetoArray(stringLine):
     arrayLine = []
     for i in stringLine.split(","):
         arrayLine.append(i)
     return arrayLine
-
-def convertRealtivePathtoAbsolutePath(relativePath):
-    currentPath = os.path.dirname(__file__)
-    return os.path.join(currentPath, relativePath)
-
-def callRetrieveData():
-    relativeDataPath = "Data/dummy_data.csv"
-    absoluteDataPath = convertRealtivePathtoAbsolutePath(relativeDataPath)
-    retrieveData(absoluteDataPath)
-
+  
 if __name__ == "__main__":
-    callRetrieveData()
-    print(dataSet)
-    
+   retrieveData()
