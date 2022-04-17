8d8b357051f779adee60ae6e627dbd0a0e7900b5
DakePeng <pengd@carleton.edu>
2022-04-17 15:27:45 -0500

    renamed dakeTest

---
+++ Tests/dakeTest.py
@@ -0,0 +1,13 @@
+import unittest
+import sys
+import os
+currentPath = os.path.dirname(__file__)
+motherdir = os.path.join(currentPath,"..")
+sys.path.append(motherdir)
+import ProductionCode
+
+class retrieveDataTest(unittest.TestCase):
+    
+
+if __name__ == '__main__':
+    unittest.main()
