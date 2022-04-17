709f2d472e667391d99db428a1530115b4048074
DakePeng <pengd@carleton.edu>
2022-04-17 14:48:52 -0500

    made data to test graphing

--- Data/MakePlotTestData.py
+++ Data/MakePlotTestData.py
@@ -4,7 +4,7 @@ with open("us_simplified.csv") as file:
     lines = file.readlines()
 numlines = len(lines)
 
-randList = range(1,200)
+randList = range(1,150)
 newfile = open("plot_test_data.csv","w")
 newfile.write(lines[0])
 for i in randList:
