98475e4d6bc53e008eef6860b5793c5213a80152
DakePeng <pengd@carleton.edu>
2022-04-17 16:46:07 -0500

    added methods to handle incorrect data

--- retrieveData.py
+++ retrieveData.py
@@ -1,8 +1,9 @@
 import sys
 import csv
 import os
- #data path of the dataset .csv file relative to this folder
-dataSet = [] #2-d array data set in the format [Date (as a tuple of form [Year, Month, Day]), County, State, Confirmed Cases, Confirmed Deaths]
+#data path of the dataset .csv file relative to this folder
+dataSet = [] 
+#2-d array data set in the format [Date (as a list of form [Year, Month, Day]), County, State, Confirmed Cases, Confirmed Deaths]
 
 def retrieveData(relativeDataPath):
     absoluteDataPath = convertRealtivePathToAbsolutePath(relativeDataPath)
@@ -25,6 +26,15 @@ def storeData(stringLines):
         arrayLine = convertStringLinetoArray(i)
         arrayLine.pop() # remove last column
         arrayLine[0] = splitDate(arrayLine[0]) #process date String and make it a list
+        
+        #skip line if formatted incorrectly
+        if(not checkDateFormat(arrayLine[0])):
+            continue
+        if(not checkNumberFormat(arrayLine[3])):
+            continue
+        if(not checkNumberFormat(arrayLine[4])):
+            continue
+        
         dataSet.append(arrayLine)
 
 def convertStringLinetoArray(stringLine):
@@ -35,4 +45,21 @@ def convertStringLinetoArray(stringLine):
 
 #Split date in to a list [Year, Month, Day]
 def splitDate(dateString):
-    return dateString.split("-")
+    dates = dateString.split("-")
+    return dates
+
+def checkDateFormat(dates):
+    try:
+        year = int(dates[0])
+        month = int(dates[1])
+        day = int(dates[2])
+    except:
+        print("Data format incorrect!")
+        
+def checkNumberFormat(num):
+    try:
+        case = int(num)
+    except:
+        print("Case number format incorrect!")
+        return False
+    return True
