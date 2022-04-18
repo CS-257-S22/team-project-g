5aff905385066f68f1ad1bb79784296448b8bb32
sealionfan <hephaestus112@gmail.com>
2022-04-18 09:39:14 -0500

    update willTest.py

--- Tests/willTest.py
+++ Tests/willTest.py
@@ -7,15 +7,17 @@ sys.path.append(motherdir)
 import getDayWithMostCases
 
 class CommandLineTest(unittest.TestCase):
-    #Test spitting out the arguments
+    # Test if state specified is in the dataSet
     def test_is_state_in_data(self):
         result = getDayWithMostCases.stateInData("Minnesota")
         self.assertTrue(result) 
 
+    # Test that dayListToStr function properly handles date list
     def test_day_list_to_str(self):
         result = getDayWithMostCases.dayListToStr(["2022", "04", "18"])
         self.assertEqual(result, "April 18, 2022")
 
+    # Test getDayWithMostCases function properly returns correct data 
     def test_get_day_with_most_cases(self):
         result = getDayWithMostCases.getDayWithMostCases("Minnesota")
         self.assertEqual(result, "On September 18, 2021 in Minnesota there were 2433 cases.")
