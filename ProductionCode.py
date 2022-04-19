bbcc831f1c54a5843fd11d7260c48225ad0dafd4
Sam-mp <mccarthys@carleton.edu>
2022-04-18 19:06:47 -0500

    finished work

--- ProductionCode.py
+++ ProductionCode.py
@@ -1,8 +1,108 @@
+from sre_compile import isstring
+import sys
+
 import retrieveData as rD
-import getDayWithMostCases as gD
+import makeGraph as mG
+import getDayWithMostCases as gDMC
 
-dummyDataSetRelativePath = "Data/dummy_data.csv"
-dataSet = rD.retrieveData(dummyDataSetRelativePath)
 
-if __name__ == "__main__":
-    gD.getDayWithMostCases("Minnesota")
+
+
+state = 0
+date = 1
+graph = 2
+help = "–state or -s “StateName” –daterange or -d “YYYY-MM-DD”’"
+
+def getStateData(stateName):
+    gDMC.getDayWithMostCases(stateName);
+
+def getDayData(dateRange):
+    print(dateRange)
+
+def makeGraphOfData(dates, caseList, Location):
+    mG.makeConfirmedCasesGraph(dates, caseList, Location)
+
+def CheckComadLine(arguments):
+    if (len(arguments) <= 0): return ("No arguments, Try -s or -d")
+    elif (len(arguments) == 1):
+        return CheckComadLineArg1(arguments)
+    elif (len(arguments) == 2) and arguments[1] != "-d":
+        return  CheckComadLineArg2(arguments)
+    elif (len(arguments) == 3):
+        return CheckComadLineArg3(arguments)
+    elif (len(arguments) == 5):
+        return CheckComadLineArg5(arguments)
+    else: return ("Not valid argument, Try: -help")
+
+def setUpValidArguments():
+    #create dictonary of commands 
+    validArguments = {
+        "-s" : state,
+        "-–state" : state,
+        "-d" : date,
+        "-–daterange" : date,
+        "-help" : help
+    }
+    return validArguments;
+
+def getComadLine():
+    #To get valid comand lines converet comand converet sys.argv into string
+    arguments = sys.argv[1:];
+    return arguments;
+
+
+def compareArgument(argument):
+    #check string aganst valid comands 
+    validArguments = setUpValidArguments()
+    if (argument in (validArguments.keys())):
+        return validArguments[argument]
+    else: return False
+    
+def checkValidDate(date):
+    #check data in put for valid year month and day 
+    DateList = date.split("-")
+    if len(DateList) == 3:
+        if (int(DateList[0]) > 2019) and (int(DateList[1]) <= 12) and (int(DateList[2]) <32): return DateList
+    return False;
+
+def CheckComadLineArg1(arguments):
+    #return correct error 
+    if compareArgument(str(arguments[0])) == state: return ("Please input state name, Try: -s texas")
+    elif compareArgument(str(arguments[0])) == date: return ("Please input date, Try: -d 2020-1-1")
+    elif compareArgument(str(arguments[0])) == help: return (help)
+    elif compareArgument(str(arguments[0])) == False: return ("Not valid argument, Try: -help")
+
+def CheckComadLineArg2(arguments):
+    #return correct error 
+    if compareArgument(str(arguments[0])) == state:
+        return getStateData(arguments[1])
+    if compareArgument(str(arguments[0])) == date:
+        if checkValidDate(str(arguments[1])) == False: return ("Please input valid date, Try: -d 2020-1-1")
+        else: return getDayData(checkValidDate(str(arguments[1])))
+
+def CheckComadLineArg3(arguments):
+    #return correct error 
+    if compareArgument(str(arguments[0])) == date: 
+        if checkValidDate(str(arguments[1])) == False: return ("Please input valid date, Try: -d 2020-1-1 2020-1-2")
+        if checkValidDate(str(arguments[2])) == False: return ("Please input valid date, Try: -d 2020-1-1 2020-1-2")
+        return getDayData([checkValidDate(str(arguments[1])),checkValidDate(str(arguments[2]))])
+    else: return ("Not valid argument, Try: -help")
+
+def CheckComadLineArg5(arguments):
+    if compareArgument(str(arguments[0])) == state and compareArgument(str(arguments[1])) == date:
+        if checkValidDate(str(arguments[3])) == False: return ("Please input valid date, Try: -d 2020-1-1")
+        if checkValidDate(str(arguments[4])) == False: return ("Please input valid date, Try: -d 2020-1-1 2020-1-2")
+        return getDayData([arguments[2],checkValidDate(str(arguments[3])),checkValidDate(str(arguments[4]))])
+
+
+
+    
+
+
+
+if __name__ == '__main__':
+    arguments = getComadLine()
+    outPut = CheckComadLine(arguments)
+    if isstring(outPut):
+        print(outPut)
+  
--- Tests/SamTest.py
+++ Tests/SamTest.py
@@ -1,29 +1,44 @@
 import unittest
-import SamProductionCode
 import sys
+import os
+
+currentPath = os.path.dirname(__file__)
+motherdir = os.path.join(currentPath,"..")
+sys.path.append(motherdir)
+
+import productionCode as SamProductionCode
+
 
 class CommandLineTest(unittest.TestCase):
     #Test if the empty comand line response works
     def testComandLineArguments(self):
+        #confirms getComadLine is working
         outPut = SamProductionCode.getComadLine();
         self.assertEqual(sys.argv[1:],outPut);
     def testEmptyComandLine(self):
+        #checks that the correct error for no arguments is given
         outPut = SamProductionCode.CheckComadLine([]);
         self.assertEqual(outPut,("No arguments, Try -s or -d"));
     def testFalseCompareArgument(self):
+        #checks that invalid arguments are false
        self.assertFalse(SamProductionCode.compareArgument(""))
     def testTrueCompareArgument(self):
+        #checks that help argument
        self.assertEqual(SamProductionCode.compareArgument("-help"),"–state or -s “StateName” –daterange or -d “YYYY-MM-DD”’")
     def testCheckValidDates(self):
+         #checks that valid dates are accepted 
         outPut = SamProductionCode.checkValidDate("2020-1-1")
         self.assertEqual(outPut,["2020","1","1"])
     def testFalseCheckValidDates(self):
+         #checks that invalid dates are not accepted 
         outPut = SamProductionCode.checkValidDate("2020-1")
         self.assertFalse(outPut)
     def testArg1(self):
+        #checks that errors for comand lines with invalid states are givien 
         outPut = SamProductionCode.CheckComadLineArg1(["-s"]);
         self.assertEqual(outPut,("Please input state name, Try: -s texas"));
     def testFullComandLine(self):
+        #checks that errors for comand lines with one invalid date is correctly givien 
        outPut = SamProductionCode.CheckComadLine(["-s","-d","texas","2020-1-1","2002-1-1"]);
        self.assertEqual(outPut,("Please input valid date, Try: -d 2020-1-1 2020-1-2"));
 
