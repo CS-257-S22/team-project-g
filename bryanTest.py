0b1d6a4837d33288edf7841fc32366abedb132bc
Bryan <102678840+0bryan0@users.noreply.github.com>
2022-04-20 02:00:39 -0500

    Add files via upload

--- bryanTest.py
+++ bryanTest.py
@@ -1,7 +1,8 @@
 import unittest
 import getTimeData
+import retrieveData
 
-
+testData = retrieveData.retrieveData("Data/dummy_data.csv")
 
 class TestSum(unittest.TestCase):
     def testDateToList(self):
@@ -9,23 +10,19 @@ class TestSum(unittest.TestCase):
         correctList = [2020, 15, 22]
         self.assertEqual(newList, correctList)
 
-    def testTrimSet(self):
-        testSet = [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20]]
-        newSet = getTimeData.trimSet(testSet, 1, 4)
-        correctSet = [[2,3,4], [7,8,9], [12,13,14], [17,18,19]]
-        self.assertEqual(newSet, correctSet)
-    
-    def testCreateReturnSet(self):
-        testSet = [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20]]
-        newSet = getTimeData.createReturnSet()
-        correctSet = [0, 1, 2, 3, 4, 5]
-        self.assertEqual(newSet, correctSet)
+    def testStrToIntList(self):
+        newList = getTimeData.strToIntList(["2020", "15", "22"])
+        correctList = [2020, 15, 22]
+        self.assertEqual(newList, correctList)
     
     def testGetTimeRange(self):
-        testSet = getTimeData.retrieveData(("Data/dummy_data.csv"))
-        newSet = getTimeData.getTimeRange("2020-04-20", "2021-06-01")
-        correctSet = getTimeData.trimSet(testSet, 0, 4)
-        self.assertEqual(newSet, correctSet)
+        testCorrect = []
+        for groupIndex in testData:
+            if groupIndex == 11 or groupIndex == 20 or groupIndex == 74:
+                testCorrect.append(testData[groupIndex])
+        testSet = testData 
+        newSet = getTimeData.getTimeRange("2021-02-17", "2021-02-20") 
+        self.assertEqual(newSet, testCorrect) 
 
     
 
