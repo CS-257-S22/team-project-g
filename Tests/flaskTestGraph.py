f3ffe8325742d50c0d72e974ad3d239eb01625a4
DakePeng <pengd@carleton.edu>
2022-04-26 22:50:37 -0500

    added tests

--- Tests/flaskTestGraph.py
+++ Tests/flaskTestGraph.py
@@ -1,48 +1,47 @@
 import unittest
-from unittest import mock
-from unittest.mock import patch
 import sys
 import os
-import io
 #access mother directory
 currentPath = os.path.dirname(__file__)
 motherdir = os.path.join(currentPath,"..")
 sys.path.append(motherdir)
-import displayGraph as dG
 from FlaskApp import *
-import checkFlaskInput as cFI
 
 class flaskTestGraph(unittest.TestCase):
 
+    #the graphs cannot be unit-checked, so all tests are integral
+    
     def testTypicalDates(self):
-        '''Integral test the case with typical input'''
+        '''Integral test of the case with typical input'''
         self.app = app.test_client()
         response = self.app.get('/Rice/Minnesota/2020-2-1/2020-9-1/graph', follow_redirects=True)
         response = str(response.data)
         isCorrect = ("Rice, Minnesota" in response) and ("2020-2-1" in response) and ("2020-9-1" in response) and ("<img src='data:image/png;base64" in response)
         self.assertTrue(isCorrect)
-    def testCheckDate(self):
-        '''test if valid input is true'''
-        self.assertTrue(cFI.checkDate("2020-2-1","2020-9-1"))
-    def testErrorCheckDate(self):
-        '''test if valid input is true'''
-        self.assertEqual('Invalid dates! Use YYYY-MM-DD format and enter dates during COVID outbreaks!',cFI.checkDate("2000-2-1","2020-9-1"))
     
-        
     def testOutofRangeDates(self):
-        '''Integraol test the case with dates that are out of range'''
+        '''Integral test of the case with dates that are out of range'''
         self.app = app.test_client()
-        response = self.app.get('/Rice/Minnesota/2020-1-1/2020-2-1', follow_redirects=True)
+        response = self.app.get('/Rice/Minnesota/2020-1-1/2020-1-20/graph', follow_redirects=True)
         response = str(response.data)
-        isCorrect = ("Rice, Minnesota" in response) and ("2020-2-1" in response) and ("2020-9-1" in response) and ("<img src='data:image/png;base64" in response)
+        isCorrect =  "Date range outside of data!" in response
         self.assertTrue(isCorrect)
         
     def testWrongOrderDates(self):
+        '''Integral test of the case with dates that are ordered incorrectly'''
         self.app = app.test_client()
-        response = self.app.get('/Rice/Minnesota/2020-4-1/2020-3-1', follow_redirects=True)
+        response = self.app.get('/Rice/Minnesota/2020-4-1/2020-3-1/graph', follow_redirects=True)
         response = str(response.data)
-        isCorrect = ("Rice, Minnesota" in response) and ("2020-2-1" in response) and ("2020-9-1" in response) and ("<img src='data:image/png;base64" in response)
-        self.assertTrue(isCorrect)    
+        isCorrect =  "Wrong order of dates!" in response
+        self.assertTrue(isCorrect)
+        
+    def testWrongLocation(self):
+        '''Integral test of the case with wrong county-state pair'''
+        self.app = app.test_client()
+        response = self.app.get('/R/M/2020-2-1/2020-3-1/graph', follow_redirects=True)
+        response = str(response.data)
+        isCorrect =  "This county-state pair does not exist! Check spelling." in response
+        self.assertTrue(isCorrect)
         
 if __name__ == '__main__':
     unittest.main()
