e1e059d421665b651e00073cf8f5ccf2460e1da5
Bryan <102678840+0bryan0@users.noreply.github.com>
2022-04-29 18:05:42 -0500

    More effecient using helper functions and datetime module.

--- getTimeData.py
+++ getTimeData.py
@@ -1,38 +1,26 @@
 import retrieveData
+import conversionFunctions as cnvFn
+import helperCheckInput as hCI
+from datetime import date
 
 dataSet = []
-  
-# crops dataSet to only consist of data within 2 dates. Both params are inclusive.
+
 def getTimeRange(date1, date2):
+
+    """ Crop dataSet to only consist of data within 2 dates. 
+
+    Both params are strings with the format 'YYYY-MM-DD, dashes included
+    Both start and end dates are inclusive. 
+    Assumes the dates inputed are within range of the data set.
+    """
+
     dataSet = retrieveData.retrieveData("Data/dummy_data.csv")
-    startDate = dateToList(date1)
-    endDate = dateToList(date2)
+    spliceStart = cnvFn.toDateTime(splitDate(date1))
+    spliceEnd - cnvFn.toDateTime(splitDate(date2))
     returnList = []
-    for dataPoint in range(len(dataSet)): 
-        currDate = strToIntList(dataSet[dataPoint][0])
-        print("currDate = ", currDate)
-        print("currDate[0] type ", type(currDate[0]))
-        print("startDate[0] type ", type(startDate[0]))
-        print("currDate[0] = ", currDate[0], " startDate[0] =", startDate[0])
-        if (currDate[0] >= startDate[0] and currDate[0] < endDate[0]):
-            print("year check hit")
-            if (currDate[1] >= startDate[1] and currDate[1] < endDate[1]):
-                print("month check hit")
-                if currDate[2] >= startDate[2] and currDate[2] < endDate[2]:
-                    print("day check hit")
-                    returnList.append(dataSet[dataPoint])                      
-    return returnList 
 
-# Converts a "YYYY-MM-DD" string to a list of [year, month, day]        
-def dateToList(date):
-    year = int(date[0:4])
-    month = int(date[5:7])
-    day = int(date[8:]) 
-    return [year, month, day] 
-
-# Converts a list of strings to ints.
-def strToIntList(dateList):
-    year = int(dateList[0])
-    month = int(dateList[1])
-    day = int(dateList[2])
-    return [year, month, day]       
+    for dataPoint in dataSet: 
+        currDate = cnvFn.toDateTime(splitDate(dataPoint[0]))
+        if (currDate >= spliceStart and currDate <= spliceEnd):
+            returnList.append(dataSet[dataPoint])                      
+    return returnList  
