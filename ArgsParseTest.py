86fcdce2d2c67dc6df8cb75ee1a5b01312072559
Sam-mp <mccarthys@carleton.edu>
2022-04-29 17:49:52 -0500

    fixed int issue

--- ArgsParseTest.py
+++ ArgsParseTest.py
@@ -5,9 +5,9 @@ parser.add_argument('County',type=str, nargs= 1,
                     help='The Name of the county to be looked up')
 parser.add_argument('State',type=str, nargs= 1,
                     help='The Name of the state to be looked up')
-parser.add_argument('StartDate',type=int, nargs=1,
+parser.add_argument('StartDate',type=str, nargs=1,
                     help='The begining of the date range to look through')
-parser.add_argument('EndDate',type=int, nargs=1,
+parser.add_argument('EndDate',type=str, nargs=1,
                     help='THe end of the date range to look through')
 parser.add_argument('-g','--graph',action='store_true',
     help= 'The flag to graph Data')
