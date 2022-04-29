cf163ae6396e7613a16835d2da88f9e0e62a84c2
Will <hephaestus112@gmail.com>
2022-04-29 17:33:40 -0500

    updated getDayWithMostCases.py docstring to match style

--- getDayWithMostCases.py
+++ getDayWithMostCases.py
@@ -2,12 +2,15 @@ import datetime
 import retrieveData
 from indexDictionary import *
 
-dataSet =[]
+dataSet = []
 
-# TO print the date in which a specific state had the highest reported cases. 
-# To do this, we iterate through the data, looking at the reported cases each day for the specified state. 
-# We would start with 0, and update a ‘highestDay’ variable if while iterating through there is a day with higher cases than our count.
+
 def getDayWithMostCases(stateName):
+    '''
+    Printsthe date in which a specific state had the highest reported cases. 
+    To do this, we iterate through the data, looking at the reported cases each day for the specified state. 
+    We would start with 0, and update a ‘highestDay’ variable if while iterating through there is a day with higher cases than our count.
+    '''
     dataSet = retrieveData.retrieveData("Data/dummy_data.csv")
     # first check if stateName is in dataSet
     if stateInData(stateName):
