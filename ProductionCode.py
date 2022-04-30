9ab0f816a080bb6b4989ebe72d153ab4dba81ca5
DakePeng <pengd@carleton.edu>
2022-04-29 21:14:57 -0500

    added to help info

--- ProductionCode.py
+++ ProductionCode.py
@@ -8,20 +8,19 @@ from conversionFunctions import *
 import displayRawData as dRD
 import argparse
 
-def CheckComadLine():
-    parser = argparse.ArgumentParser(description='Process some integers.')
+def CheckComadLine(argv):
+    parser = argparse.ArgumentParser(description='Please enter the following in the order they are shown for raw data, and add -g for graphs.')
     parser.add_argument('County',type=str,
                         help='The Name of the county to be looked up')
     parser.add_argument('State',type=str,
                         help='The Name of the state to be looked up')
     parser.add_argument('StartDate',type=str,
-                        help='The begining of the date range to look through')
+                        help='The begining of the date range to look through, in the format YYYY-MM-DD')
     parser.add_argument('EndDate',type=str,
-                        help='THe end of the date range to look through')
+                        help='THe end of the date range to look through, in the format YYYY-MM-DD')
     parser.add_argument('-g','--graph',action='store_true',
         help= 'The flag to graph Data')
-
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
     return args
 
 def callData(args):
@@ -37,7 +36,7 @@ def callData(args):
 
     
 if __name__ == '__main__':
-    outPut = callData(CheckComadLine())
+    outPut = callData(CheckComadLine(sys.argv[1:]))
     outPut = outPut.split('<br/>')
     if isinstance(outPut,str):
        print(outPut)
