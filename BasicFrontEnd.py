968ab0bb56c47a251f9465848acda0a826027867
DakePeng <pengd@carleton.edu>
2022-05-04 09:31:33 -0500

    added templates

--- BasicFrontEnd.py
+++ BasicFrontEnd.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template, request
-
+import displayGraph as dG
 #some helper functions to convert data to the inputs that our main code take
 from conversionFunctions import *
 
@@ -8,27 +8,6 @@ import csv
 import sys
 
 app = Flask(__name__)
-
-#Mock input
-mockLocation = ["Rice", "Minnesota"]
-mockDateRange = [['2021','2','1'], ['2021','3','1']]
-
-#Mock Case data and date data from the dataset
-mockDates = [['2021', '02', '01'], ['2021', '02', '02'], ['2021', '02', '03'], ['2021', '02', '04'], 
-             ['2021', '02', '05'], ['2021', '02', '06'], ['2021', '02', '07'], ['2021', '02', '08'], 
-             ['2021', '02', '09'], ['2021', '02', '10'], ['2021', '02', '11'], ['2021', '02', '12'], 
-             ['2021', '02', '13'], ['2021', '02', '14'], ['2021', '02', '15'], ['2021', '02', '16'], 
-             ['2021', '02', '17'], ['2021', '02', '18'], ['2021', '02', '19'], ['2021', '02', '20'], 
-             ['2021', '02', '21'], ['2021', '02', '22'], ['2021', '02', '23'], ['2021', '02', '24'], 
-             ['2021', '02', '25'], ['2021', '02', '26'], ['2021', '02', '27'], ['2021', '02', '28'], 
-             ['2021', '03', '01']]
-
-mockConfirmedCases = [6408, 6415, 6419, 6452, 6475, 6487, 6506, 6510, 6522, 6535, 6551, 6566, 6583, 6588, 
-                      6594, 6598, 6601, 6614, 6622, 6629, 6632, 6638, 6641, 6642, 6653, 6664, 6671, 6690, 6700]
-
-mockConfirmedDeaths = [78, 78, 82, 82, 82, 82, 82, 82, 83, 84, 84, 87, 87, 87, 87, 87, 87, 88, 
-                       89, 89, 90, 90, 90, 91, 91, 91, 91, 91, 91]
-
 
 @app.route('/')
 def homepage():
@@ -69,14 +48,12 @@ def getGraph(location, dateRange):
     '''
     renders a page for graphs based on mock data
     '''
-    return render_template('graph.html', location = mockLocation, dateRange = mockDateRange, data = "mockOutput.png")
+    return dG.displayGraph(location, dateRange)
 
 def getRawData(location, dateRange):
-    '''
-    renders a page for raw data based on mock data
-    '''
-    return render_template('rawdata.html', location = mockLocation, dateRange = mockDateRange, dates = mockDates, 
-                           confirmedCases = mockConfirmedCases, confirmedDeaths = mockConfirmedDeaths)
+    
+    return render_template('rawdata.html', location = location, dateRange = dateRange)
+
 @app.errorhandler(404)
 def page_not_found(e):
     '''
